9df758502dd34c839d87c9eeec4a7ae9051b468c
Flo <flononymus@gmail.com>
2024-07-03 11:24:34 +0200

    updates renderer

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -15,8 +15,7 @@ import Test from './pages/Test'
 
 export type Page = 'Home' | 'Settings' | 'Buttons' | 'Spinner' | 'Particles' | 'Switches' | 'Tether' | 'Ball' | 'Joystick' | 'Test';
 
-const startPage = "Ball"
-
+const startPage = "Switches"
 
 declare global {
   interface Window {
